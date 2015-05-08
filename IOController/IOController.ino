1a3c11b10d76524b327a818fb847c118f58604bc
esper0328 <esper0328@gmail.com>
2015-05-08 21:29:16 +0900

    calculate delta

--- IOController/IOController.ino
+++ IOController/IOController.ino
@@ -9,12 +9,13 @@ unsigned long init_norm = 0;
 #define ANALOG_PIN_Z 5
 #define EFFECT_START_SIGNAL 1
 #define EFFECT_STOP_SIGNAL 2
+#define DELAY_TIME 50
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(SPEED);
   initializeSensor();
-  initializeActuator();  
-  Serial.begin(SPEED);  
+  initializeActuator();
 }
 
 void initializeSensor()
@@ -23,16 +24,10 @@ void initializeSensor()
   init_y = analogRead(ANALOG_PIN_Y);
   init_z = analogRead(ANALOG_PIN_Z);
   init_norm = sqrt(init_x * init_x + init_y * init_y + init_z * init_z);
-/*
-  Serial.print("X\t");
-  Serial.println(init_x);
-  Serial.print("Y\t");
-  Serial.println(init_y);
-  Serial.print("Z\t");
-  Serial.println(init_z);  
-  Serial.print("NORM\t");
-  Serial.println(init_norm);
-*/
+  Serial.print("delta_x\t");
+  Serial.print("delta_y\t");
+  Serial.print("delta_z\t");
+  Serial.print("delta_norm");
 }
 
 void initializeActuator()
@@ -44,7 +39,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   sensorLoop();
   actuatorLoop();
-  delay(1000);
+  delay(DELAY_TIME);
 }
 
 void sensorLoop()
@@ -53,34 +48,40 @@ void sensorLoop()
   unsigned long y = 0;
   unsigned long z = 0;
   unsigned long norm = 0;
-  unsigned long diff = 0;
+  unsigned long delta_norm = 0;
+  unsigned long delta_x = 0;
+  unsigned long delta_y = 0;
+  unsigned long delta_z = 0;
   const unsigned long thresh = 20;
   
-  x = analogRead(3);
-  y = analogRead(4);
-  z = analogRead(5);
+  x = analogRead(ANALOG_PIN_X);
+  y = analogRead(ANALOG_PIN_Y);
+  z = analogRead(ANALOG_PIN_Z);
   norm = sqrt(x*x + y*y + z*z);
-  diff = getDifference(norm, init_norm);
+  delta_norm = getDifference(norm, init_norm);
+  delta_x = getDifference(x, init_x);
+  delta_y = getDifference(y, init_y);
+  delta_z = getDifference(z, init_z);
   
-  if(diff > thresh){
-    //Serial.print("HA\t");
-    //Serial.print(diff);
-    unsigned int effect_parameter = diff/10;
-    Serial.print(effect_parameter);
-    Serial.print('\n');
-    //Serial.println(norm);
-    //Serial.println(init_norm);
+  if(delta_norm > thresh){
+    notifyEffect();
   }
-/*
-  Serial.print("X\t");
-  Serial.println(x);
-  Serial.print("Y\t");
-  Serial.println(y);
-  Serial.print("Z\t");
-  Serial.println(z);  
-  Serial.print("NORM\t");
-  Serial.println(norm);
-*/
+
+  Serial.print(delta_x);
+  Serial.print("\t");
+  Serial.print(delta_y);
+  Serial.print("\t");
+  Serial.print(delta_z);  
+  Serial.print("\t");
+  Serial.print(delta_norm);
+  Serial.print("\n");
+}
+
+void notifyEffect()
+{
+  unsigned int effect_parameter = delta_norm/10;
+  Serial.print(effect_parameter);
+  Serial.print('\n');
 }
 
 unsigned long getDifference(unsigned long rv_a, unsigned long rv_b)
