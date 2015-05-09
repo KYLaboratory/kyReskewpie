23fa7e735d9a98855563e76958698e61abe19cd6
honeypon <hogehoge@hogehoge.com>
2015-05-09 14:36:22 +0900

    RGB LED and is_notify

--- IOController/IOController.ino
+++ IOController/IOController.ino
@@ -1,11 +1,9 @@
 #include <Grove_LED_Bar.h>
-#include <Servo.h>
+#include <ChainableLED.h>
 
 #define OUTPUT_SOLENOID 4
-#define INPUT_BUTTON 6
+//#define INPUT_BUTTON 6
 #define OUTPUT_VIBRATOR 2
-#define OUTPUT_SERVO 3
-
 
 #define OUTPUT_LED 12
 #define SPEED 9600
@@ -45,19 +43,20 @@ void notifyEffect(const struct vector3D& param1, const struct vector3D& param2, 
 struct vector3D initParam1 = {0, 0, 0, 0};
 struct vector3D initParam2 = {0, 0, 0, 0};
 
+bool is_notify = true;  //notifyEffect() and actuatorLoop()
+
+
 //actuation
 int LEDbarValue = 3;//LED bar value
 int solenoidState = HIGH;//solenoid
 int HP; //hit point 
 
 Grove_LED_Bar bar(9, 8, 0);  // Clock pin, Data pin, Orientation
-
-Servo myservo;  // create servo object to control a servo
+ChainableLED leds(6, 7, NUM_LEDS);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(SPEED);
-  myservo.attach(OUTPUT_SERVO);
   initializeSensor();
   initializeActuator();
 }
@@ -72,13 +71,11 @@ void initializeSensor()
 {  
   initParam1 = createVector3D(analogRead(ANALOG_PIN_X1), analogRead(ANALOG_PIN_Y1), analogRead(ANALOG_PIN_Z1));
   initParam2 = createVector3D(analogRead(ANALOG_PIN_X2), analogRead(ANALOG_PIN_Y2), analogRead(ANALOG_PIN_Z2));
-
-  /*
+  
   Serial.print("delta_x\t");
   Serial.print("delta_y\t");
   Serial.print("delta_z\t");
   Serial.print("delta_norm\n");
-  */
 }
 
 void initializeActuator()
@@ -86,6 +83,7 @@ void initializeActuator()
   pinMode(OUTPUT_SOLENOID, OUTPUT);
   pinMode(OUTPUT_VIBRATOR, OUTPUT);
   pinMode(INPUT_BUTTON, INPUT);
+  leds.init();
   
 }
 
@@ -137,12 +135,11 @@ struct vector3D calcDiffVector3D(const struct vector3D& currentParam, const stru
 
 void notifyEffect(const struct vector3D& param1, const struct vector3D& param2, const ARMS_STATUS arms)
 {  
-  static bool is_notify = true;
-  const unsigned long max = 468;
+
+  
   if(arms == ARMS_DOUBLE)
   {
     if(is_notify){
-      /*
       Serial.print(param1.x);
       Serial.print("\t");
       Serial.print(param1.y);
@@ -160,17 +157,12 @@ void notifyEffect(const struct vector3D& param1, const struct vector3D& param2, 
       Serial.print(param2.norm);
       Serial.print("\t");
       Serial.println("DOUBLE");
-      */
-      Serial.print("d,");
-      Serial.print(param1.x * 100 / max);
-      Serial.print("\n");
       is_notify = false;
     }
   }
   else if(arms == ARMS_SINGLE)
   {
     if(is_notify){
-      /*
       Serial.print(param1.x);
       Serial.print("\t");
       Serial.print(param1.y);
@@ -188,17 +180,6 @@ void notifyEffect(const struct vector3D& param1, const struct vector3D& param2, 
       Serial.print(param2.norm);
       Serial.print("\t");
       Serial.println("SINGLE");
-      */
-      if(param1.x < param2.x){
-        Serial.print("s,");
-        Serial.print(param2.x * 100 / max);
-        Serial.print("\n");
-      }
-      else {
-        Serial.print("s,");
-        Serial.print(param1.x * 100 / max);
-        Serial.print("\n");
-      }
       is_notify = false;
     }
   }
@@ -207,7 +188,7 @@ void notifyEffect(const struct vector3D& param1, const struct vector3D& param2, 
     const unsigned int wait_thresh = 100;
     static unsigned int wait_count = 0;
     if(wait_count == wait_thresh){
-      //Serial.println("READY");
+      Serial.println("READY");
       is_notify = true;
       wait_count = 0;
     }
@@ -239,47 +220,49 @@ void actuatorLoop()// LEDbarのみの記述
     }
     bar.setLevel(LEDbarValue);
     // ここまで　をIOControllerにマージする。
+    
+    // if(is_notify)でシングルLED光らせる　else シングルLED消灯
+    // hogehoge
 }
 
 
 void serialEvent() {  
+  const int DELAY_TIME_LEDbar=300;  //micro second
+  const int DELAY_TIME_VIBRATOR=300;  //micro second
   if (Serial.read() == EFFECT_STOP_SIGNAL) {
     if(HP<=0){
       digitalWrite(OUTPUT_SOLENOID, HIGH);
       LEDbarValue = 0;
       bar.setLevel(LEDbarValue);
-      delay(300);
+      delay(DELAY_TIME_LEDbar);
       digitalWrite(OUTPUT_SOLENOID, LOW);
-      myservo.write(180); //サーボを動かす(180度)
-      delay(2000);
-      myservo.write(0);
-      
       effectLED();
     }else if(HP>0){
       digitalWrite(OUTPUT_VIBRATOR, HIGH);
-      delay(300);
+      delay(DELAY_TIME_VIBRATOR);
       digitalWrite(OUTPUT_VIBRATOR, LOW);      
     }
   }
 }
 
 void effectLED(){
+  const int DELAY_TIME_EFFECTLED = 700;
   int k;
   for(k=0;k<3;k++){
     // Turn on LEDs 1, 2, 3, 4, 5
     // 0b000000000011111 == 0x1F
     bar.setBits(0b000001010101010);
-    delay(700);
+    delay(DELAY_TIME_EFFECTLED);
   
     // Turn on LEDs 6, 7, 8, 9, 10
     // 0b000001111100000 == 0x3E0
     bar.setBits(0b000000101010101);
-    delay(700);
+    delay(DELAY_TIME_EFFECTLED);
   }
   for(k=0;k<3;k++){
     bar.setBits(0b000001111111111);
-    delay(800);
+    delay(DELAY_TIME_EFFECTLED);
     bar.setBits(0b000000000000000);
-    delay(800);
+    delay(DELAY_TIME_EFFECTLED);
   }
 }
